5d5720a57379a0d4748577351f2af149b66eb2fc
Lucian Williams <Lucian.Williams@my.utsa.edu>
2022-07-21 21:14:30 -0500

    Added Item Table

---
+++ Summer-2022-CS3773-WebApp/scripts/createItem.sql
@@ -0,0 +1,10 @@
+-- createItem.sql
+drop table if exists Item;
+CREATE TABLE `Item` (
+  `id`              int         NOT NULL,
+  `name`            varchar(30) NOT NULL,
+  `stock`           int         DEFAULT 0 NOT NULL, 
+  `pricecents`      int         NOT NULL,
+  `image`           blob        DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
