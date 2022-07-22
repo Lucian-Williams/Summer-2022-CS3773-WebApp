04c075c4104bd0bd44503ef25a48e065e7fa20e3
Lucian Williams <Lucian.Williams@my.utsa.edu>
2022-07-21 20:45:43 -0500

    Added SQL DB, Fixed Customer

--- Summer-2022-CS3773-WebApp/scripts/createCustomer.sql
+++ Summer-2022-CS3773-WebApp/scripts/createCustomer.sql
@@ -3,8 +3,8 @@ drop table if exists Customer;
 CREATE TABLE `Customer` (
   `id`		    int		NOT NULL,
   `name`	    varchar(30) DEFAULT NULL,
-  `baseLoc`         char(2)     DEFAULT NULL, 
+  `state`           char(2)     DEFAULT NULL, 
   `birthDt`         date        DEFAULT NULL,
   `gender`          char(1)     DEFAULT 'U',  
-  PRIMARY KEY (`custNr`)
+  PRIMARY KEY (`id`)
 );
