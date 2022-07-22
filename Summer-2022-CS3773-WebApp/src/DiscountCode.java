79ca2378bd3ca0ffd14a7f39ec71dd15ccec5855
Anthrac1t3 <coleholub@protonmail.com>
2022-07-21 20:17:49 -0500

    Added basic classes

---
+++ Summer-2022-CS3773-WebApp/src/DiscountCode.java
@@ -0,0 +1,9 @@
+public class DiscountCode {
+    private int code;
+    private int percentage;
+
+    @Override
+    public String toString() {
+        return "DiscountCode{}";
+    }
+}
---
+++ Summer-2022-CS3773-WebApp/src/User.java
@@ -0,0 +1,9 @@
+public class User {
+    private int ID;
+    private String name;
+
+    @Override
+    public String toString() {
+        return "User{}";
+    }
+}
