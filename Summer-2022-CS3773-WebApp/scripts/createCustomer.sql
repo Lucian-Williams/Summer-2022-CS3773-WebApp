6b2bdff9da9931ae608a9f5cca179603154e0031
Lucian Williams <Lucian.Williams@my.utsa.edu>
2022-07-21 21:45:50 -0500

    Added Orders Table

--- Summer-2022-CS3773-WebApp/scripts/createCustomer.sql
+++ Summer-2022-CS3773-WebApp/scripts/createCustomer.sql
@@ -1,11 +1,11 @@
 -- createCustomer.sql
 drop table if exists Customer;
 CREATE TABLE `Customer` (
-  `id`		    int		NOT NULL,
-  `name`	    varchar(30) DEFAULT NULL,
+  `id`              int         NOT NULL,
+  `name`            varchar(30) DEFAULT NULL,
   `state`           char(2)     DEFAULT NULL, 
   `birthDt`         date        DEFAULT NULL,
-  `creatDt`	    date	NOT NULL,
+  `creatDt`         date        NOT NULL,
   `gender`          char(1)     DEFAULT 'U',  
   PRIMARY KEY (`id`)
 );
---
+++ Summer-2022-CS3773-WebApp/scripts/createTOrder.sql
@@ -0,0 +1,11 @@
+-- createTOrder.sql
+drop table if exists TOrder;
+CREATE TABLE `TOrder` (
+  `id`              int         NOT NULL,
+  `custId`          int         NOT NULL,
+  `totalPriceCents` int         NOT NULL,
+  `status`          int         NOT NULL,
+  `orderDt`         date        NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`custId`) REFERENCES Customer (`id`)
+);
