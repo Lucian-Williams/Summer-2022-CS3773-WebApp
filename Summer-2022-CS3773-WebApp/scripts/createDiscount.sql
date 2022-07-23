c0e843eb743f5047ae2fbecd93935b4ffa8e0f36
Lucian Williams <Lucian.Williams@my.utsa.edu>
2022-07-23 14:23:00 -0500

    Updated Discount
    Realized Discount Codes can only ever apply to one order at a time, so
    Discount now has an orderId field.

--- Summer-2022-CS3773-WebApp/scripts/createDiscount.sql
+++ Summer-2022-CS3773-WebApp/scripts/createDiscount.sql
@@ -2,9 +2,11 @@
 drop table if exists Discount;
 CREATE TABLE `Discount` (
   `code`            int         NOT NULL,
+  `orderId`         int         DEFAULT NULL,
   `percentOff`      int         NOT NULL,
   `maxDollarAmount` int         NOT NULL,
   `status`          int         NOT NULL,
   `expireDt`        date        NOT NULL,
-  PRIMARY KEY (`code`)
+  PRIMARY KEY (`code`),
+  FOREIGN KEY (`orderId`) REFERENCES TOrder (`id`)
 );
