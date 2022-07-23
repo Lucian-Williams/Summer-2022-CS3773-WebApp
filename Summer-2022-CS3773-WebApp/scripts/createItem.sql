0146e4cbe215145d3065084bfaa85aa41ffde68d
Lucian Williams <Lucian.Williams@my.utsa.edu>
2022-07-23 14:11:14 -0500

    Updated Item
    Now has itemType.

--- Summer-2022-CS3773-WebApp/scripts/createItem.sql
+++ Summer-2022-CS3773-WebApp/scripts/createItem.sql
@@ -3,6 +3,7 @@ drop table if exists Item;
 CREATE TABLE `Item` (
   `id`              int         NOT NULL,
   `name`            varchar(30) NOT NULL,
+  `itemType`        int         NOT NULL,
   `stock`           int         DEFAULT 0 NOT NULL, 
   `pricecents`      int         NOT NULL,
   `image`           blob        DEFAULT NULL,
