cd0517ff396a4a055c7bad281a50485e78e5fa8c
Lucian Williams <Lucian.Williams@my.utsa.edu>
2022-07-23 14:20:47 -0500

    Updated ItemsToOrders
    Added quantity field.

--- Summer-2022-CS3773-WebApp/scripts/createItemsToOrders.sql
+++ Summer-2022-CS3773-WebApp/scripts/createItemsToOrders.sql
@@ -3,6 +3,7 @@ drop table if exists ItemsToOrders;
 CREATE TABLE `ItemsToOrders` (
   `itemId`          int         NOT NULL,
   `orderId`         int         NOT NULL,
+  `quantity`        int         NOT NULL DEFAULT 1,
   PRIMARY KEY (`itemId`, `orderId`),
   FOREIGN KEY (`itemId`) REFERENCES Item (`id`),
   FOREIGN KEY (`orderId`) REFERENCES TOrder (`id`)
