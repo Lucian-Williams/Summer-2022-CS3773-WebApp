747628bc62e4bbde928d4cd64996b737bfde1290
Lucian Williams <Lucian.Williams@my.utsa.edu>
2022-07-25 20:52:49 -0500

    Updated Admin Table
    Added salt for more secure password verifiers.

--- Summer-2022-CS3773-WebApp/scripts/createAdmin.sql
+++ Summer-2022-CS3773-WebApp/scripts/createAdmin.sql
@@ -3,6 +3,7 @@ drop table if exists Admin;
 CREATE TABLE `Admin` (
   `id`              int         NOT NULL,
   `name`            varchar(30) NOT NULL,
-  `pwHash`          int         NOT NULL,
+  `salt`            int         NOT NULL,
+  `verifier`        int         NOT NULL,
   PRIMARY KEY (`id`)
 );
