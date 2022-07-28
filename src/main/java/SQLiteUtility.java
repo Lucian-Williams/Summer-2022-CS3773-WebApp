8cc467e41860ddf067b76846c1c1099cafcb4f1a
Lucian Williams <Lucian.Williams@my.utsa.edu>
2022-07-28 18:26:46 -0500

    Added SQLUtility.java
    Also added static void main to Main class.

---
+++ src/main/java/SQLiteUtility.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+import java.sql.SQLException;
+
+public class SQLiteUtility {
+    private static Connection connection = null;
+    private static Statement statement = null;
+    private static PreparedStatement preparedStatement = null;
+    private static ResultSet resultSet = null;
+
+    public static void connect(String url, String user, String password) throws Exception {
+        connection = DriverManager.getConnection(url, user, password);
+        connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+    }
+
+    public static void disconnect() throws Exception {
+        connection.close();
+    }
+
+    public static void insertItem(Item item) {
+
+    }
+}
