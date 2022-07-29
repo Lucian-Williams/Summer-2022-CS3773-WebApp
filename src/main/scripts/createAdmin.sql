516cdadde18cc77d3e0a041bff9ef1ecb1232a5f
Anthrac1t3 <coleholub@protonmail.com>
2022-07-28 19:16:12 -0500

    Working UI

--- src/main/scripts/createAdmin.sql
+++
@@ -1,8 +0,0 @@
--- createAdmin.sql
-drop table if exists Admin;
-CREATE TABLE `Admin` (
-  `id`              integer     NOT NULL,
-  `name`            text        NOT NULL, -- use name as salt
-  `verifier`        integer     NOT NULL,
-  PRIMARY KEY (`id`)
-);
