99728516acb32fc7046ca4e6acda422db71a0f17
ReneRod22 <100320874+ReneRod22@users.noreply.github.com>
2022-08-01 23:03:14 -0500

    Update Sale.java

--- src/main/java/Sale.java
+++ src/main/java/Sale.java
@@ -1,9 +1,15 @@
+//class Sale focuses on how a sale works, such as the percentage of the sale and the start and end date of the sale
 public class Sale {
     private int itemId;
     private int percentOff;
     private String startDt;
     private String expireDt;
-
+/**
+* @param itemId Reads the ID of the item
+* @param percentOff Takes the percentage off of the item in the sale
+* @param startDt Reads the start date of the sale
+* @param expireDt Reads the expiration date of the sale
+*/
     public Sale(int itemId, int percentOff, String startDt, String expireDt) {
         this.itemId = itemId;
         this.percentOff = percentOff;
