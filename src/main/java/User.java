e9bf1e126bfd228bb3b68b79dc333dcd6291f918
ReneRod22 <100320874+ReneRod22@users.noreply.github.com>
2022-08-01 23:06:02 -0500

    Update User.java

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,7 +1,12 @@
+// class User takes the information about the user
+/**
+* @param ID Reads and takes the ID of the user
+* @param name Reads and takes the name of the user
+*/
 public class User {
     private int ID;
     private String name;
-
+// Returns class User and the information taken
     @Override
     public String toString() {
         return "User{}";
